a9fcc96621c76c8057470f9f1d751a0fd8329ef5
candycoatedrox <60051903+candycoatedrox@users.noreply.github.com>
2025-10-15 23:24:20 -0400

    ShopTest.java and bugLog.java complete

--- ShopTest.java
+++ ShopTest.java
@@ -3,6 +3,186 @@ import org.junit.Test;
 
 public class ShopTest {
 
-    // add your tests here 
+    // no need to organically find bugs via tests --
+    // make tests to catch the bugs you see in the code!!
+
+    // COMPUTER TESTS
+    @Test
+    public void testConstructorMemory() {
+        Computer testComputer = new Computer(
+            "Mac Pro (Late 2013)",
+            "3.5 GHc 6-Core Intel Xeon E5",
+            1024, 64,
+            "macOS Big Sur", 2013, 1500
+        );
+
+        assertEquals(64, testComputer.memory);
+    }
+
+    @Test
+    public void testConstructorPrice() {
+        Computer testComputer = new Computer(
+            "Mac Pro (Late 2013)",
+            "3.5 GHc 6-Core Intel Xeon E5",
+            1024, 64,
+            "macOS Big Sur", 2013, 1500
+        );
+
+        assertEquals(1500, testComputer.price);
+    }
+
+    @Test
+    public void testSetOS() {
+        Computer testComputer = new Computer(
+            "Mac Pro (Late 2013)",
+            "3.5 GHc 6-Core Intel Xeon E5",
+            1024, 64,
+            "macOS Big Sur", 2013, 1500
+        );
+
+        testComputer.setOS("MacOS Monterey");
+        assertEquals("MacOS Monterey", testComputer.operatingSystem);
+    }
+
+    // RESALESHOP TESTS
+    @Test
+    public void testShopConstructor() {
+        ResaleShop testShop = new ResaleShop();
+
+        assertTrue(testShop.inventory.isEmpty());
+    }
+
+    @Test
+    public void testBuy() {
+        ResaleShop testShop = new ResaleShop();
+        Computer testComputer = new Computer(
+            "Mac Pro (Late 2013)",
+            "3.5 GHc 6-Core Intel Xeon E5",
+            1024, 64,
+            "macOS Big Sur", 2013, 1500
+        );
+
+        try {
+            testShop.buy(testComputer);
+        } catch (Exception e) {
+        }
+
+        assertTrue(testShop.inventory.contains(testComputer));
+    }
+    
+    // If I try to test for exceptions using (expected =), the program throws an error
+    // This is definitely related to my general JUnit issues, but I can't figure out
+    // how to fix it (see reflection.md)
+    // I'm using a workaround in the next few tests instead
+    @Test
+    public void testBuyException() {
+        ResaleShop testShop = new ResaleShop();
+        Computer testComputer = new Computer(
+            "Mac Pro (Late 2013)",
+            "3.5 GHc 6-Core Intel Xeon E5",
+            1024, 64,
+            "macOS Big Sur", 2013, 1500
+        );
+
+        boolean exceptionThrown = false;
+        // Workaround: If buy() throws an Exception,
+        // exceptionThrown is set to true and the test passes
+        try {
+            testShop.buy(testComputer);
+            testShop.buy(testComputer);
+        } catch (Exception e) {
+            exceptionThrown = true;
+        }
+
+        assertTrue(exceptionThrown);
+    }
+
+    @Test
+    public void testSellException() {
+        ResaleShop testShop = new ResaleShop();
+        Computer testComputer = new Computer(
+            "Mac Pro (Late 2013)",
+            "3.5 GHc 6-Core Intel Xeon E5",
+            1024, 64,
+            "macOS Big Sur", 2013, 1500
+        );
+
+        boolean exceptionThrown = false;
+        // Workaround: If sell() throws an Exception,
+        // exceptionThrown is set to true and the test passes
+        try {
+            testShop.sell(testComputer);
+        } catch (Exception e) {
+            exceptionThrown = true;
+        }
+
+        assertTrue(exceptionThrown);
+    }
+
+    @Test
+    public void testPrintInventoryLoop() {
+        ResaleShop testShop = new ResaleShop();
+
+        boolean exceptionThrown = false;
+        // Workaround: If printInventory() throws an IndexOutOfBoundsException,
+        // exceptionThrown is set to true and the test fails
+        try {
+            testShop.printInventory();
+        } catch (IndexOutOfBoundsException e) {
+            exceptionThrown = true;
+        }
+
+        assertFalse(exceptionThrown);
+    }
+
+    @Test
+    public void testRefurbishPrice() {
+        ResaleShop testShop = new ResaleShop();
+        Computer testComputer = new Computer(
+            "Mac Pro (Late 2010)",
+            "3.5 GHc 6-Core Intel Xeon E5",
+            1024, 64,
+            "macOS Big Sur", 2010, 1500
+        );
+
+        // Manually bypass buy(), since it doesn't work
+        testShop.inventory.add(testComputer);
+
+        try {
+            testShop.refurbish(testComputer, "None");
+        } catch (Exception e) {
+        }
+
+        assertEquals(250, testComputer.price);
+    }
+
+    @Test
+    public void testRefurbishOS() {
+        ResaleShop testShop = new ResaleShop();
+        Computer testComputer = new Computer(
+            "Mac Pro (Late 2013)",
+            "3.5 GHc 6-Core Intel Xeon E5",
+            1024, 64,
+            "macOS Big Sur", 2013, 1500
+        );
+
+        // Manually bypass buy(), since it doesn't work
+        testShop.inventory.add(testComputer);
+
+        try {
+            // If refurbish() were using .equals,
+            // this wouldn't change testComputer.operatingSystem
+            testShop.refurbish(testComputer, new String("None"));
+        } catch (Exception e) {
+        }
+
+        assertEquals("macOS Big Sur", testComputer.operatingSystem);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Running");
+        ShopTest runTests = new ShopTest();
+        runTests.testRefurbishOS();
+    }
     
 }
--- bugLog.md
+++ bugLog.md
@@ -1,39 +1,39 @@
 ## Bug 1
-Brief description:  
-Failed unit test: 
+Brief description: The Computer constructor sets memory to 16 instead of the provided value. 
+Failed unit test: testConstructorMemory()
 
 ## Bug 2
-Brief description:  
-Failed unit test: 
+Brief description: The Computer constructor sets price to 0 instead of the provided value.
+Failed unit test: testConstructorPrice()
 
 ## Bug 3
-Brief description:  
-Failed unit test: 
+Brief description: The setOS method sets operating system to "None" instead of the provided value.
+Failed unit test: testSetOS()
 
 ## Bug 4
-Brief description:  
-Failed unit test: 
+Brief description: Instances of ResaleShop are initialized with a computer already in the inventory.
+Failed unit test: testShopConstructor()
 
 ## Bug 5
-Brief description:  
-Failed unit test: 
+Brief description: The buy method always adds the same arbitrarily defined computer to the inventory.
+Failed unit test: testBuy()
 
 ## Bug 6
-Brief description:  
-Failed unit test: 
+Brief description: The buy method fails to throw an Exception if the given computer is already in the inventory.
+Failed unit test: testBuyException()
 
 ## Bug 7
-Brief description:  
-Failed unit test: 
+Brief description: The sell method fails to throw an Exception if the given computer isn't in the inventory.
+Failed unit test: testSellException()
 
 ## Bug 8
-Brief description:  
-Failed unit test: 
+Brief description: The printInventory method loops through the inventory while its current index is *less than or equal to* instead of *less than* the length of the list.
+Failed unit test: testPrintInventoryLoop()
 
 ## Bug 9
-Brief description:  
-Failed unit test: 
+Brief description: The refurbish method sets price based on which bracket the computer's yearMade falls into; the price set for a computer from 2000-2011 breaks the pattern of the other brackets and seems to have an extra 0 on the end.
+Failed unit test: testRefurbishPrice()
 
 ## Bug 10
-Brief description:  
-Failed unit test: 
+Brief description: The refurbish method compares strings with != instead of .equals().
+Failed unit test: testRefurbishOS()
